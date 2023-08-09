5a48609246002ef39458418e25965f95b15a6890
Heinz-Alexander Fuetterer <fuetterh@posteo.de>
2023-08-09 15:25:19 +0200

    feat!: make cli optional

--- src/services_test4/__main__.py
+++ src/services_test4/__main__.py
@@ -1,3 +1,11 @@
+import sys
+
+try:
+    import typer  # noqa: F401
+except ImportError:
+    print("Please install 'services-test4[cli]' to use this command.")
+    sys.exit(0)
+
 from services_test4.cli import app
 
 app(prog_name="services-test4")
