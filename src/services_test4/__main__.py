65c36a89e88bb98b40732a6dcd1f12c94cd23404
Heinz-Alexander Fuetterer <fuetterh@posteo.de>
2023-09-28 11:03:45 +0200

    feat: cli

--- src/services_test4/__main__.py
+++ src/services_test4/__main__.py
@@ -1,11 +1,9 @@
 import sys
 
 try:
-    import typer  # noqa: F401
+    from services_test4.cli import app
+
+    app(prog_name="services-test4")
 except ImportError:
     print("Please install 'services-test4[cli]' to use this command.")
-    sys.exit(0)
-
-from services_test4.cli import app
-
-app(prog_name="services-test4")
+    sys.exit(1)
